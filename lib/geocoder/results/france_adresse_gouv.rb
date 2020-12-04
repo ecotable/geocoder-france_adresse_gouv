c2974e37f2c16414d9c98e4a1db8ce1f558d01f1
Adrien Siami <adrien@siami.fr>
2020-12-04 09:49:27 +0100

    State map to french regions, introduce departments

--- lib/geocoder/results/france_adresse_gouv.rb
+++ lib/geocoder/results/france_adresse_gouv.rb
@@ -1,6 +1,27 @@
 module Geocoder
   module Result
     class FranceAdresseGouv < Geocoder::Result::Base
+      STATE_CODE_MAPPINGS = {
+        "Guadeloupe" => "01",
+        "Martinique" => "02",
+        "Guyane" => "03",
+        "La Réunion" => "04",
+        "Mayotte" => "06",
+        "Île-de-France" => "11",
+        "Centre-Val de Loire" => "24",
+        "Bourgogne-Franche-Comté" => "27",
+        "Normandie" => "28",
+        "Hauts-de-France" => "32",
+        "Grand Est" => "44",
+        "Pays de la Loire" => "52",
+        "Bretagne" => "53",
+        "Nouvelle-Aquitaine" => "75",
+        "Occitanie" => "76",
+        "Auvergne-Rhône-Alpes" => "84",
+        "Provence-Alpes-Côte d'Azur" => "93",
+        "Corse" => "94"
+      }.freeze
+
       def address
         data['properties']['label']
       end
@@ -17,12 +38,20 @@ module Geocoder
         data['properties']['city']
       end
 
-      def state_code
+      def department_code
         data['properties']['context'].split(', ')[0]
       end
 
-      def state
+      def department
         data['properties']['context'].split(', ')[1]
+      end
+
+      def state
+        data['properties']['context'].split(', ').last
+      end
+
+      def state_code
+        STATE_CODE_MAPPINGS[state]
       end
 
       def country
@@ -32,7 +61,6 @@ module Geocoder
       def country_code
         "FR"
       end
-
     end
   end
 end
